ff05f6462c59079779466f4c2bdc406092d4b65e
Jorge <nflux-jmartinez@nflux.ai>
2020-03-10 11:11:50 -0700

    Just need to modify the schema a little bit

---
+++ README.md
@@ -0,0 +1,8 @@
+This is going to be a basic login page using Flask as the back end and some HTML and CSS in the Front End.
+
+
+You'll need to install flask and flask-sqlalchemy
+
+There is a requirements.txt that you can use to set up a virtual environment by running
+
+pip install requirements.txt
---
+++ flaskblog/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '08c35dc5534510722c45cbc8445411ae'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+print(app.config['MAIL_USERNAME'])
+print(app.config['MAIL_PASSWORD'])
+mail = Mail(app)
+
+
+from flaskblog import routes
